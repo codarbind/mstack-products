80b30b624de21c2d3be7849a1843ec8df73ca49d
Abdwahab Adekola <adekolaabdwahababisoye@gmail.com>
2023-02-18 03:54:22 +0700

    testing -patch

--- test/units/api.test.ts
+++ test/units/api.test.ts
@@ -39,7 +39,7 @@ describe("GET /product/?id=**", () => {
     testProd = await request(baseURL).post("/product/add").send(newProduct);
   });
   afterAll(async () => {
-    await request(baseURL).delete(`/product/?id=${testProd.body.data["_id"]}`);
+    await request(baseURL).delete(`/product/${testProd.body.data["_id"]}`);
   });
 
   it("should return 200", async () => {
@@ -57,6 +57,56 @@ describe("GET /product/?id=**", () => {
     expect(response.body.data).toHaveProperty("name", "testGEt");
     expect(response.body.data).toHaveProperty("description", "testgeting dihs");
     expect(response.body.data).toHaveProperty("price", 349);
+    expect(response.body.data).toHaveProperty("deleted", false);
+    expect(response.body.data).toHaveProperty("createdAt");
+    expect(response.body.data).toHaveProperty("updatedAt");
+    expect(response.body.data).toHaveProperty("__v", 0);
+  });
+});
+
+describe("PATCH /product", () => {
+  let testProd: Request;
+  const newProduct = {
+    name: "testGEtach",
+    description: "testgeting dihshirt",
+    price: 349,
+  };
+
+  beforeAll(async () => {
+    // set up the Product
+    testProd = await request(baseURL).post("/product/add").send(newProduct);
+  });
+  afterAll(async () => {
+    await request(baseURL).delete(`/product/?id=${testProd.body.data["_id"]}`);
+  });
+
+  it("should return 200", async () => {
+    let response = await request(baseURL)
+      .patch(`product/${testProd.body.data["_id"]}`)
+      .send({
+        name: "updated-name",
+        description: "should be extensively described",
+        price: 23457,
+      });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("should update the product", async () => {
+    let response = await request(baseURL)
+      .patch(`product/${testProd.body.data["_id"]}`)
+      .send({
+        name: "updated-name",
+        description: "should be extensively described",
+        price: 23457,
+      });
+    console.log({ respObj: response });
+    expect(response.body.data).toHaveProperty("_id", testProd.body.data["_id"]);
+    expect(response.body.data).toHaveProperty("name", "updated-name");
+    expect(response.body.data).toHaveProperty(
+      "description",
+      "should be extensively described"
+    );
+    expect(response.body.data).toHaveProperty("price", 23457);
     expect(response.body.data).toHaveProperty("deleted", false);
     expect(response.body.data).toHaveProperty("createdAt");
     expect(response.body.data).toHaveProperty("updatedAt");
